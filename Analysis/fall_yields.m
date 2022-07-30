b4817e43be89b673100489d87159e65b74b6a7cf
Nelson Barnett <nelsonvbarnett@gmail.com>
2022-07-29 23:36:45 -0400

    Improve read-in

--- Analysis/fall_yields.m
+++ Analysis/fall_yields.m
@@ -2,19 +2,34 @@
 % 
 % Get averages and mins of falls and non falls based on percent yields
 
-ratio = load('../Data/Gaitcycles combined n20000g0.014_0.019p0.02_0.5/fallratio1419NEWFALL.csv');
-yield = load('../Data/Gaitcycles combined n20000g0.014_0.019p0.02_0.5/percentYield1419NEWFALL.csv');
+%% Load data
+filename = ['../Data/Gaitcycles data n20000g0.016_0.019p0.02_0.5d01-Jun22/' ...
+    'gaitCyclesProcessed0.014_0.019_0.02_0.5.csv'];
 
+full_data = readtable(filename);
+
+% How many perturbations at each gamma. 
+% Should always be the same so only need first value.
+[pert_tot, ~] = find(diff(table2array(full_data(:,1))) ~= 0, 1, 'first');
+
+% Get gamma values from table.
+gam = unique(table2array(full_data(:,1)));
+
+ratio = reshape(table2array(full_data(:,3)), [pert_tot, length(gam)]);
+yield = reshape(table2array(full_data(:,4)), [pert_tot, length(gam)]);
+
+%% Calculate
 falls = ratio ./ (ratio + 1);
 nonfalls = 1 - falls;
 
 percentFalls = falls.*yield;
 percentNonfalls = nonfalls.*yield;
 
-disp(['average % nonfalls: ',num2str(mean(percentNonfalls,'all'))])
+%% Print
+disp(['average % nonfalls: ',num2str(mean(percentNonfalls,'all')), '%'])
 
-disp(['min % of nonfalls: ',num2str(min(percentNonfalls,[],'all'))])
+disp(['min % of nonfalls: ',num2str(min(percentNonfalls,[],'all')), '%'])
 
-disp(['average % falls: ',num2str(mean(percentFalls,'all'))])
+disp(['average % falls: ',num2str(mean(percentFalls,'all')), '%'])
 
-disp(['min % of falls: ',num2str(min(percentFalls,[],'all'))])
+disp(['min % of falls: ',num2str(min(percentFalls,[],'all')), '%'])
